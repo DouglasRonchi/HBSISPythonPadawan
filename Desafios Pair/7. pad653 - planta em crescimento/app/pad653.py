93c39fee2e946edd3a0dc35ae5199953b73a2b5b
DouglasRonchi <douglasronchi02@hotmail.com>
2020-01-07 07:46:47 -0300

    Alterando Growing Plant

--- Desafios Pair/7. pad653 - planta em crescimento/app/pad653.py
+++ Desafios Pair/7. pad653 - planta em crescimento/app/pad653.py
@@ -1,20 +1,34 @@
+class Plant:
+    def __init__(self):
+        self.up_speed = 0
+        self.down_speed = 0
+        self.desired_height = 0
+        self.plant_height = 0
+        self.count = 1
+        self.days = 0
 
-def growing_plant(up_speed, down_speed, desired_height):
-    plant_height = 0
-    count = 1
+    def growing_plant(self, up_speed, down_speed, desired_height):
+        self.plant_height = 0
+        self.count = 1
 
-    if 5 >= up_speed >= 100:
-        raise Exception('UpSpeed must be between 5 and 100')
-    if 2 >= down_speed > up_speed:
-        raise Exception('DownSpeed must be between 2 and UpSpeed value')
-    if 4 >= desired_height >= 1000:
-        raise Exception('DownSpeed must be between 4 and 1000')
+        self._rules(up_speed, down_speed, desired_height)
 
-    while plant_height < desired_height:
-        if count % 2 == 1:
-            plant_height += up_speed
-        else:
-            plant_height -= down_speed
-        count += 1
-    days = int(count / 2)
-    return days
+        while self.plant_height < desired_height:
+            if self.count % 2 == 1:
+                self.plant_height += up_speed
+            else:
+                self.plant_height -= down_speed
+            self.count += 1
+        self.days = int(self.count / 2)
+        return self.days
+
+    def _rules(self, up_speed, down_speed, desired_height):
+        if up_speed <= 5 or up_speed >= 100:
+            raise Exception('UpSpeed must be between 5 and 100')
+        if down_speed <= 2 or down_speed >= up_speed:
+            raise Exception('DownSpeed must be between 2 and UpSpeed value')
+        if desired_height <= 4 or desired_height >= 1000:
+            raise Exception('DownSpeed must be between 4 and 1000')
+
+plant = Plant()
+plant.growing_plant(200, 10, 910)
